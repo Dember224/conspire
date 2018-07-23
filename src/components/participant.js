c50b6ce3dd8bb1a7ffcb35a00c76bb48fa74ce9d
Antoine <Dember224@gmail.com>
2018-07-23 03:15:23 -0400

    This solution for button independence doesn't work but feels like the right track so I'm commiting.

--- src/components/participant.js
+++ src/components/participant.js
@@ -7,26 +7,36 @@ export class Participant extends React.Component{
   constructor(props){
     super(props);
     //setting initial score
-    this.state = {
-      score: 0,
-    }
+  this.state = {
+    points: [
+      {score: 0},
+      {player: 1}
+  ]
+  }
+
+
     this.increaseScore = this.increaseScore.bind(this);
   };
 //gives more points to a player
   increaseScore(morePoints) {
-    let currentScore = this.state.score
+    let currentScore = this.state.points
     this.setState({
-      score: currentScore + 1
+      points: currentScore.map((player) =>{
+        if (player === this.props.key){
+          score: currentScore + 1
+        }
+      }
+      )
     });
   }
 
   render() {
     const playerNumber = [1, 2, 3, 4]
     return (
-//iterates through and makes 4 score keeping buttons
+//iterates through and makes 4 score keeping buttons (try ternary operator on the onClick prop. if this.props.player === i)
       <ul>
         {playerNumber.map(i => {
-          return <li key={i}>player:{i}<Score score={this.state.score} onClick={ (i) => this.increaseScore(i)} player={i} /></li>
+          return <li key={i}>player:{i}<Score score={this.state.score} onClick={ (i) => this.increaseScore(i) } key={i} /></li>
         })}
       </ul>
     )
